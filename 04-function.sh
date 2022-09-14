1f83741c0cb351dbae67ab69c027cf8e0ee4d4a3
Samdevops789 <Sampathbandla2@gmail.com>
2022-09-14 23:35:09 +0530

    updating ./learning-devops7/learning-devops7

--- 04-function.sh
+++ 04-function.sh
@@ -3,7 +3,11 @@
 ## Declaring a function
 Sample() {
   echo Sample Function
+  echo a from main program= $a
+  b=20
 }
 
 ## Accessing a function, simply function name
+a=10
 Sample
+echo b from function=$b
