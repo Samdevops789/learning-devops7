72b19932e7dc1de50f96d1605fa7a9b6be80881d
Samdevops789 <Sampathbandla2@gmail.com>
2022-09-19 17:14:00 +0530

    updating ./learning-devops7/learning-devops7

--- 06-inputs.sh
+++ 06-inputs.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+if [ $# -lt 2 ]; then
+  echo Missing Inputs
+  exit 1
+  fi
+  
 echo First Argument= $1
 echo Second Argument= $2
 echo All arguments =$@
 echo Number of Arguments= $#
-  
