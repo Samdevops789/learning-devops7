146de0f6d56964ba2b80a75a88b0aa44a9b4dc7c
Samdevops789 <Sampathbandla2@gmail.com>
2022-09-19 17:35:26 +0530

    updating ./learning-devops7/learning-devops7

---
+++ 07-case.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+case $1 in
+     apple)
+       echo 'price= $1'
+       ;;
+     $2 in
+     banana)
+       echo 'price= $2'
+       ;;
+     *)
+       echo 'Item not found'
+       ;;
+     esac
